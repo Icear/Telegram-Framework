fc421bb490db6a6967b3db4675d6e3ad04acba14
Icear <Icear@users.noreply.github.com>
2018-05-01 18:42:19 +0800

    add proxy support add okhttp3 package

--- src/main/java/indi/telegramframwork/context/telegrambot/TelegramBotContext.java
+++ src/main/java/indi/telegramframwork/context/telegrambot/TelegramBotContext.java
@@ -1,21 +1,15 @@
 package indi.telegramframwork.context.telegrambot;
 
 import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.Update;
-import indi.telegramframwork.context.telegrambot.event.TelegramBotEvent;
-import org.springframework.beans.factory.annotation.Autowired;
+import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 
 /**
@@ -24,15 +18,49 @@ import java.util.stream.Collectors;
 @Component
 public class TelegramBotContext {
     @Value("#{setting['telegramBot.token']}")
-    protected String telegramBotToken;
+    private String telegramBotToken;
+    @Value("#{setting['proxy']}")
+    private boolean proxySwitch;
+    @Value("#{setting['proxy.type']}")
+    private String proxyTypeString;
+    @Value("#{setting['proxy.address']}")
+    private String proxyAddress;
+    @Value("#{setting['proxy.port']}")
+    private int port;
+
     private Logger logger = LogManager.getLogger(TelegramBotContext.class);
 
     @Bean
     public TelegramBot getTelegramBot() {
         logger.info("connecting to telegram bot service via token...");
         logger.trace("telegramBot.token = " + telegramBotToken);
-        TelegramBot telegramBot = new TelegramBot(telegramBotToken);
-        logger.debug("telegram bot service connect result: " + telegramBot.toString());
+        TelegramBot telegramBot;
+        if (proxySwitch) {
+            logger.info("proxy on, proxy " + proxyTypeString + " to " + proxyAddress + ":" + port);
+            //转换proxyType
+            Proxy.Type proxyType = Proxy.Type.HTTP;
+            switch (proxyTypeString) {
+                case "http":
+                    proxyType = Proxy.Type.HTTP;
+                    break;
+                case "sock":
+                    proxyType = Proxy.Type.SOCKS;
+                    break;
+                default:
+                    logger.error("unsupported proxy type " + proxyTypeString + ", use default: HTTP");
+            }
+
+            telegramBot = new TelegramBot.Builder(telegramBotToken)
+                    .okHttpClient(
+                            new OkHttpClient.Builder()
+                                    .proxy(new Proxy(proxyType, new InetSocketAddress(proxyAddress, port)))
+                                    .build()
+                    ).build();
+        } else {
+            telegramBot = new TelegramBot(telegramBotToken);
+        }
+        logger.info("telegram bot service connected");
+        logger.debug("telegram bot service connect result: " + telegramBot);
         return telegramBot;
     }
 
